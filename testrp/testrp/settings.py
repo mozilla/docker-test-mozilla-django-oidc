0b3bda168bc4f711570970b332a81f028ccd1c7a
Andreas Lutro <anlutro@gmail.com>
2018-05-16 13:43:17 +0200

    implement DRF in testrp

--- testrp/testrp/settings.py
+++ testrp/testrp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'mozilla_django_oidc',
     'testrp'
 ]
@@ -127,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'AUTHENTICATION_CLASSES': [
+        'mozilla_django_oidc.drf.OIDCAuthentication'
+    ]
+}
 
 
 # OIDC SETUP
--- testrp/testrp/urls.py
+++ testrp/testrp/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 
-from .views import HomePageView
+from .views import HomePageView, TestAPIView
 
 
 urlpatterns = [
     url(r'^oidc/', include('mozilla_django_oidc.urls')),
+    url(r'^api/$', TestAPIView.as_view(), name='api'),
     url(r'^$', HomePageView.as_view(), name='home')
 ]
