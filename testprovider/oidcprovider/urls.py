f7b136044c450b684536dfc13e7aabd250834c88
Will Kahn-Greene <willkg@mozilla.com>
2022-03-28 13:00:48 -0400

    Update to Django 3.2.12, remove pinax-theme-bootstrap, add bootstrap 5.1.3 (#30)
    This removes pinax-theme-bootstrap because it hasn't been maintained in
    a long time. This adds a couple of forms needed by django-user-accounts,
    conntects everything together, and redoes the css for the site.

--- testprovider/oidcprovider/urls.py
+++ testprovider/oidcprovider/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import include, path
 
 from .views import HomePageView
 
 urlpatterns = [
-    url(r'^openid/', include('oidc_provider.urls', namespace='oidc_provider')),
-    url(r'^account/', include('account.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', HomePageView.as_view(), name='home'),
-
+    path("openid/", include("oidc_provider.urls", namespace="oidc_provider")),
+    # path('account/', include('django.contrib.auth.urls')),
+    path('account/', include("account.urls")),
+    path("admin/", admin.site.urls),
+    path("", HomePageView.as_view(), name="home"),
 ]
