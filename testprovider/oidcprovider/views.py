e588c941cdfa7bb7db6c1f16faf7105d32214e71
Tasos Katsoulas <akatsoulas@gmail.com>
2022-11-08 14:22:24 +0200

    Restore delete functionality

--- testprovider/oidcprovider/views.py
+++ testprovider/oidcprovider/views.py
@@ -1,5 +1,10 @@
 from django.views.generic.base import TemplateView
+from django.contrib.auth.models import User
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
+def delete_user(request):
+    User.objects.all().delete()
